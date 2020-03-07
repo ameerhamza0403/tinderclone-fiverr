7ce65e70822b60745f50f4341f7ee5b16a1bb983
Shahbaz haider <shahbazhaider5297@gmail.com>
2020-03-06 23:31:01 -0800

    splash created

---
+++ app/screens/SplashScreen/index.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { Button, View, Text,StatusBar,Image} from 'react-native';
+
+import styles from './style'
+import {
+    BallIndicator,
+    BarIndicator,
+    DotIndicator,
+    MaterialIndicator,
+    PacmanIndicator,
+    PulseIndicator,
+    SkypeIndicator,
+    UIActivityIndicator,
+    WaveIndicator,
+  } from 'react-native-indicators';
+
+
+
+
+export default class SplashScreen extends Component {
+    render (){
+
+    return (
+        <View style={styles.SplashContaner}>
+          <StatusBar
+            backgroundColor="#FF655B"
+            barStyle="light-content"></StatusBar>
+          <View style={{marginTop:0,width:150 ,height:200}}>
+            <Image  style={styles.logo} source={require('../../../assests/images/fireicon.png')}/>
+          </View>
+  
+          <View style={styles.textCon}>
+            <Text style={styles.text}> Tinder  </Text>
+          </View>
+  
+      
+            <View style={styles.BottomContainer}>
+             <SkypeIndicator color="white"/>
+            </View>
+          
+        </View>
+      );
+  
+    }
+}
---
+++ app/screens/SplashScreen/style.js
@@ -0,0 +1,30 @@
+export default {
+    SplashContaner: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#FF655B',
+      },
+      BottomContainer: {
+       width:20,
+       height:20,
+       
+       backgroundColor: '#FF655B',
+        alignItems: 'center',
+      },
+      logo: {
+        
+        width: 150,
+        height: 200,
+      },
+      textCon: {
+        marginTop:0
+      },
+      text: {
+        color: 'white',
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginBottom: 150,
+      },
+    
+    }
