b8d1a4776510dccd409818e0944dbcae29ff8279
Shahbaz Haider <39879330+shahbazhaider@users.noreply.github.com>
2020-05-27 11:58:33 -0700

    update

--- app/screens/SettingsScreen/index.js
+++ app/screens/SettingsScreen/index.js
@@ -13,22 +13,18 @@ import {
   Switch,
   TouchableHighlight,
   SafeAreaView,
-  Modal
-  
+  Modal,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';  
+import Icon from 'react-native-vector-icons/Ionicons';
 import {ProgressSteps, ProgressStep} from 'react-native-progress-steps';
 
 import styles from './style';
 
 import {Card} from 'native-base';
 import Slider from '@react-native-community/slider';
-import MultiSlider from '@ptomasroos/react-native-multi-slider'
-
-
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
 export default class EditProfileInfo extends React.Component {
-
   state = {switchValue: false, sliderValue: 1};
   toggleSwitch = value => {
     //onValueChange of the switch this function will be called
@@ -37,243 +33,162 @@ export default class EditProfileInfo extends React.Component {
     //which will result in re-render the text
   };
 
-
-
   render() {
     return (
       <ScrollView>
         <View style={styles.mainContainer}>
-          <StatusBar
-            backgroundColor="#FF4A00FF"
-            barStyle="light-content"></StatusBar>
-
-
-
-
-
-      
+          <StatusBar backgroundColor="#FF4A00FF" barStyle="light-content" />
 
           <View style={styles.viewContainer}>
+            <View
+              style={{
+                flex: 6,
+                justifyContent: 'flex-end',
+                marginTop: 20,
+                marginBottom: 10,
+                flexDirection: 'column',
+                width: '90%',
+                marginLeft: '1%',
+              }}>
+              <View style={{flex: 1}}>
+                <View
+               style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                    }}>
+                    Payment Account
+                  </Text>
+                </View>
+                <View style={{flexDirection: 'row', marginTop: 10}}>
+                  <View style={{flex: 20}}>
+                    <Text style={{fontSize: 17}}>Manage Payment Account</Text>
+                  </View>
 
+                  <View style={{flex: 1}}>
+                    <Icon
+                      name={'ios-arrow-forward'}
+                      color={'#ababab'}
+                      size={25}
+                    />
+                  </View>
+                </View>
+              </View>
 
-            <View style={{flex:6,justifyContent: 'flex-end',marginTop:20, marginBottom: 10,flexDirection: 'column', width: '90%', marginLeft: '1%'}}>
-              
-              
-              <View style={{flex:1}}>
-                            
-              <View
-                              style={{flex:8,marginTop: 10, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                              <Text
-                                style={{
-                                
-                              
-                                  fontSize: 20,
-                                  fontWeight: 'bold',
-                                }}>
-                               Payment Account
-                              </Text>
-                            </View>
-              <View style={{flexDirection:'row',marginTop:10}}>
-                <View style={{flex:20,}}>
-                  <Text style={{fontSize:17}}>
-                    Manage Payment Account 
+              <View style={{flex: 1}}>
+                <View
+              style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                    }}>
+                    Contact us
+                  </Text>
+                </View>
+                <View style={{flexDirection: 'row', marginTop: 10}}>
+                  <View style={{flex: 20, alignItems: 'center'}}>
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                      }}>
+                      Help and Support
+                    </Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={{flex: 1}}>
+                <View
+                style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                    }}>
+                    Community
+                  </Text>
+                </View>
+                <View style={{marginTop: 0}}>
+                  <View style={{flex: 20}}>
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontWeight: 'bold',
+                        color: '#444544',
+                      }}>
+                      Community Guidelines
+                    </Text>
+                  </View>
+
+                  <View style={{flex: 20}}>
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontWeight: 'bold',
+                        color: '#444544',
+                      }}>
+                      Safety Tips
+                    </Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={{flex: 1}}>
+                <View
+                  style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                      color: '#FF4A00FF',
+                    }}>
+                    Legal
                   </Text>
                 </View>
 
-                <View style={{flex:1}}>
-                <Icon  
-                             name={'ios-arrow-forward'}  
-                              color={'#ababab'}  
-                             size={25}  
-                       /> 
+                <View
+                style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                      color: '#ABABAB',
+                    }}>
+                    Licenses
+                  </Text>
+                </View>
+
+                <View
+               style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                      color: '#ABABAB',
+                    }}>
+                    Privacy Policy
+                  </Text>
+                </View>
+
+                <View
+                style={styles.itemsContainer}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: 'bold',
+                      color: '#ABABAB',
+                    }}>
+                    Terms of Services
+                  </Text>
                 </View>
               </View>
-                       
-              </View>
+            </View>
+          </View>
 
-
-              <View style={{flex:1}}>
-                            
-                            
-                            <View
-                                            style={{flex:8,marginTop: 10, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                              }}>
-                                           Contact us
-                                            </Text>
-                                          </View>
-                            <View style={{flexDirection:'row',marginTop:10}}>
-                              <View style={{flex:20,alignItems:'center'}}>
-                              <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                              }}>
-                                          Help and Support 
-                                            </Text>
-                                      
-                              </View>
-              
-                             
-                            </View>
-                                     
-                            </View>
-
-
-
-                            <View style={{flex:1}}>
-                            
-                            
-                            <View
-                                            style={{flex:8,marginTop: 10, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                              }}>
-                                         Community
-                                            </Text>
-                                          </View>
-                            <View style={{marginTop:0}}>
-                              <View style={{flex:20,}}>
-                              <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 17,
-                                                fontWeight: 'bold',
-                                                color:'#444544'
-                                           
-                                              }}>
-                                       Community Guidelines 
-                                            </Text>
-                                      
-                              </View>
-              
-                              <View style={{flex:20,}}>
-                              <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 17,
-                                                fontWeight: 'bold',
-                                                color:'#444544'
-                                               
-                                              }}>
-                                        Safety Tips
-                                            </Text>
-                                      
-                              </View>
-              
-                             
-                            </View>
-                                     
-                            </View>
-
-
-
-                            
-                            <View style={{flex:1}}>
-                            
-                            
-                            <View
-                                            style={{flex:8,marginTop: 30, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                                color:'#FF4A00FF'
-                                              }}>
-                                   Legal
-                                            </Text>
-                                          </View>
-                           
-
-
-                                          <View
-                                            style={{flex:8,marginTop: 30, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                                color:'#ABABAB'
-                                              }}>
-                                     Licenses
-                                            </Text>
-                                          </View>
-                           
-
-
-                                          <View
-                                            style={{flex:8,marginTop: 30, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                                color:'#ABABAB'
-                                              }}>
-                                      Privacy Policy
-                                            </Text>
-                                          </View>
-                           
-
-
-                                          <View
-                                            style={{flex:8,marginTop: 30, marginBottom: 5,ali:'center',justifyContent:'center'}}>
-                                            <Text
-                                              style={{
-                                              
-                                            
-                                                fontSize: 20,
-                                                fontWeight: 'bold',
-                                                color:'#ABABAB'
-                                              }}>
-                                   Terms of Services
-                                            </Text>
-                                          </View>
-                           
-
-                           
-
-                                     
-                            </View>
-
-              
-              
-             
-                          
-                            </View>
-
-
-
-
-
-
-</View>
-
-           
-
-        
-
-
-
-
-   <View style={{alignItems: 'center', marginTop: 0, flex: 2}}>
+          <View style={{alignItems: 'center', marginTop: 0, flex: 2}}>
             <View style={styles.roundBtn}>
               <TouchableHighlight
                 style={{alignItems: 'center'}}
@@ -289,12 +204,12 @@ export default class EditProfileInfo extends React.Component {
     );
   }
 }
-EditProfileInfo.navigationOptions={  
-  tabBarIcon:({tintColor, focused})=>(  
-      <Icon  
-          name={focused ? 'settings' : 'settings'}  
-          color={tintColor}  
-          size={25}  
-      />  
-  )  
-}  
+EditProfileInfo.navigationOptions = {
+  tabBarIcon: ({tintColor, focused}) => (
+    <Icon
+      name={focused ? 'settings' : 'settings'}
+      color={tintColor}
+      size={25}
+    />
+  ),
+};
--- app/screens/SettingsScreen/style.js
+++ app/screens/SettingsScreen/style.js
@@ -10,92 +10,12 @@ export default {
     padding: 2,
     flex:6
   },
-  input: {
-    width: 300,
-    height: 50,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  },
-  heading: {
-    fontSize: 18,
-    color: '#3F3F3F',
-    marginLeft: 7,
-  },
-  headerText: {
-    fontSize: 20,
-    color: 'black',
-    marginTop: 15,
-    marginLeft: 20,
-  },
-  detailText: {
-    marginLeft: 7,
-    color: '#ABABAB',
-  },
-
-  imageView: {
-    width: 125,
-    margin: 5,
-    height: 190,
-    borderRadius: 10,
-    backgroundColor: '#e7e7e7',
-  },
-  iconStyle: {
-    width: 30,
-    height: 35,
-  },
-  imageStyle: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-  },
-  roundBtnTrans: {
-    borderColor: 'black',
-    borderRadius: 30,
-    width: '85%',
-    marginTop: 20,
-    height: 40,
-
-    flexDirection: 'row',
-    alignItems: 'center',
+  itemsContainer:{
+    flex: 8,
+    marginTop: 30,
+    marginBottom: 5,
+    ali: 'center',
     justifyContent: 'center',
-    borderBottomWidth: 1,
-    borderTopWidth: 1,
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
   },
   roundBtn: {
     borderColor: 'white',
@@ -113,73 +33,5 @@ export default {
     borderRightWidth: 1,
   },
 
-  MainContainer :{
-    
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: (Platform.OS == 'ios') ? 20 : 0
-    
-    },
-    
-    ModalInsideView:{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 22
-    
-    },
-    
-    TextStyle:{
-    
-      fontSize: 20, 
-      marginBottom: 20, 
-      color: "#fff",
-      padding: 20,
-      textAlign: 'center'
-    
-    },    
-  danceBtn: {
-    marginLeft:5,
-    borderColor: 'white',
-    borderRadius: 15,
-    width: '22%',
-    marginTop: 20,
-    height: 45,
-    backgroundColor: '#FF4A00FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  textInput: {
-    alignSelf: 'stretch',
-    fontSize: 14,
-    color: '#676666',
-
-    // Add this to specify bottom border thickness
-  },
-  BottomContainer: {
-    width: 20,
-    height: 20,
-
-    backgroundColor: '#FF655B',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 150,
-    height: 200,
-  },
-  textCon: {
-    marginTop: 0,
-  },
-  text: {
-    color: 'white',
-    fontSize: 35,
-    fontWeight: 'bold',
-    marginBottom: 150,
-  },
-  des:{
-    color:'#ABABAB',
-    marginLeft:5
-  }
+  
 };
