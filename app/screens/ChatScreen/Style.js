3101decbd7a52b1bd24d6115270d059e8d428364
Shahbaz Haider <39879330+shahbazhaider@users.noreply.github.com>
2020-06-21 03:54:33 -0700

    update

---
+++ app/screens/ChatScreen/Style.js
@@ -0,0 +1,25 @@
+
+
+import { size } from '../../helpers/devices';
+import * as Statics from '../../helpers/statics';
+export default {
+container: {
+    flex: 1,
+    paddingRight: 5,
+    paddingLeft: 5,
+    width:'100%',
+    alignItems: 'center',
+    backgroundColor: '#e0ebeb',
+},
+circle: {
+    backgroundColor: '#fff',
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    marginLeft: 5,
+    marginTop: 10,
+},
+circleText: {
+    flex: 1, color: '#fff', fontSize: 15,
+},
+}
