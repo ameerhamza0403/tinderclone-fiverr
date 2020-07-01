6aeb8a66213b8f71e9b37e5e2469dd5b051de11a
Shahbaz Haider <39879330+shahbazhaider@users.noreply.github.com>
2020-07-01 12:04:10 -0700

    update

--- app/screens/SwipeProfileScreen/index.js
+++ app/screens/SwipeProfileScreen/index.js
@@ -57,161 +57,139 @@ class SwipeCards extends React.Component {
       currentDate: '',
 
       cards: [
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F1.jpg?alt=media&token=d554d673-c044-410a-8a21-b6b093299527',
-          },
-        },
-
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F2.jpg?alt=media&token=403dad12-51b2-47ea-942a-34d5881fa0b6',
-          },
-        },
-
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F3.jpg?alt=media&token=11269f00-b091-40dc-9d53-e962dca2740b',
-          },
-        },
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F4.jpg?alt=media&token=151f5340-bc49-4fe4-bee5-3ef8da291edf',
-          },
-        },
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F5.jpg?alt=media&token=c7d88a2b-a6f2-41b8-b037-e1d7f038b172',
-          },
-        },
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F6.jpg?alt=media&token=b9750f33-9986-4163-a6cd-53850618146b',
-          },
-        },
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F7.jpg?alt=media&token=27060360-08fe-45db-b3e0-e1aed7c91f5e',
-          },
-        },
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F8.jpg?alt=media&token=6c097fb6-f733-43c9-8826-9e486aa85931',
-          },
-        },
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F9.jpg?alt=media&token=d2133450-eb87-473b-9dfc-910d06262fb8',
-          },
-        },
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F10.jpg?alt=media&token=4e953cbc-de3f-41e8-ada4-9739dd2f8508',
-          },
-        },
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F11.jpg?alt=media&token=144c172a-73be-4678-9883-d4d5aded916e',
-          },
-        },
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F12.jpg?alt=media&token=da1013ef-bd8e-4581-98b0-2b1cdc8ff8e3',
-          },
-        },
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F14.jpg?alt=media&token=e87b6e59-c22c-45ab-ba2b-6f48c997d3cc',
-          },
-        },
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F15.jpg?alt=media&token=17ba499b-ad69-4b24-885d-c3d64638d905',
-          },
-        },
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F16.jpg?alt=media&token=75aadbbc-5517-41e1-a9fa-8ee5e081f8d8',
-          },
-        },
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F17.jpg?alt=media&token=eefd7136-bc0e-425d-8458-3ce90a8db2e3',
-          },
-        },
-        {
-          text: 'Saye',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F18.jpg?alt=media&token=53ca5d9c-0c62-48bf-b167-681d0de05bd4',
-          },
-        },
-        {
-          text: 'Hamza',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F19.jpg?alt=media&token=b1c5cc9a-1b6a-49dd-8653-229988549878',
-          },
-        },
-
-        {
-          text: 'Ebigs',
-
-          image: {
-            uri:
-              'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F20.jpg?alt=media&token=618db6e6-d986-46b7-a73a-b6f52e6093b8',
-          },
-        },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F1.jpg?alt=media&token=d554d673-c044-410a-8a21-b6b093299527',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F2.jpg?alt=media&token=403dad12-51b2-47ea-942a-34d5881fa0b6',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F3.jpg?alt=media&token=11269f00-b091-40dc-9d53-e962dca2740b',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F4.jpg?alt=media&token=151f5340-bc49-4fe4-bee5-3ef8da291edf',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F5.jpg?alt=media&token=c7d88a2b-a6f2-41b8-b037-e1d7f038b172',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F6.jpg?alt=media&token=b9750f33-9986-4163-a6cd-53850618146b',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F7.jpg?alt=media&token=27060360-08fe-45db-b3e0-e1aed7c91f5e',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F8.jpg?alt=media&token=6c097fb6-f733-43c9-8826-9e486aa85931',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F9.jpg?alt=media&token=d2133450-eb87-473b-9dfc-910d06262fb8',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F10.jpg?alt=media&token=4e953cbc-de3f-41e8-ada4-9739dd2f8508',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F11.jpg?alt=media&token=144c172a-73be-4678-9883-d4d5aded916e',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F12.jpg?alt=media&token=da1013ef-bd8e-4581-98b0-2b1cdc8ff8e3',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F14.jpg?alt=media&token=e87b6e59-c22c-45ab-ba2b-6f48c997d3cc',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F15.jpg?alt=media&token=17ba499b-ad69-4b24-885d-c3d64638d905',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F16.jpg?alt=media&token=75aadbbc-5517-41e1-a9fa-8ee5e081f8d8',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F17.jpg?alt=media&token=eefd7136-bc0e-425d-8458-3ce90a8db2e3',
+        //   },
+        // },
+        // {
+        //   text: 'Saye',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F18.jpg?alt=media&token=53ca5d9c-0c62-48bf-b167-681d0de05bd4',
+        //   },
+        // },
+        // {
+        //   text: 'Hamza',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F19.jpg?alt=media&token=b1c5cc9a-1b6a-49dd-8653-229988549878',
+        //   },
+        // },
+        // {
+        //   text: 'Ebigs',
+        //   image: {
+        //     uri:
+        //       'https://firebasestorage.googleapis.com/v0/b/ebigs-tinder.appspot.com/o/General%2FGallery%2F20.jpg?alt=media&token=618db6e6-d986-46b7-a73a-b6f52e6093b8',
+        //   },
+        // },
       ],
     };
   }
@@ -234,8 +212,6 @@ class SwipeCards extends React.Component {
     var difference_In_Time = todayDate.getTime() - accDate.getTime();
     var difference_In_Days = difference_In_Time / (1000 * 3600 * 24);
 
-
-
     if (difference_In_Days <= 7) {
       Alert.alert('Your have trial, Congrats! Its a Dutch');
       this.props.navigation.navigate('MatchScreen');
@@ -243,15 +219,13 @@ class SwipeCards extends React.Component {
         ModalVisibleStatus: false,
       });
     } else {
-
       if (this.state.payment) {
         Alert.alert('You are a subscriber, Congrats! Its a Dutch');
         this.props.navigation.navigate('MatchScreen');
         this.setState({
           ModalVisibleStatus: false,
         });
-      }
-      else  {
+      } else {
         Alert.alert(
           'You have no trial or you are not a subscriber ,Please Pay',
         );
@@ -259,17 +233,12 @@ class SwipeCards extends React.Component {
         this.setState({
           ModalVisibleStatus: false,
         });
-      } 
-     
+      }
     }
-
-    
   };
   state = {switchValue: false, sliderValue: 1};
   toggleSwitch = value => {
- 
     this.setState({switchValue: value});
-  
   };
 
   showModalFunction(visible) {
@@ -278,6 +247,8 @@ class SwipeCards extends React.Component {
     this.setState({ModalVisibleStatus: visible});
   }
   async componentDidMount() {
+    let myList = [];
+    const that = this;
     console.disableYellowBox = true;
     const id = await AsyncStorage.getItem('id', 0);
     console.log(id);
@@ -288,11 +259,36 @@ class SwipeCards extends React.Component {
       });
 
       database()
+        .ref('/Users/')
+        .on('value', querySnapShot => {
+          let data = querySnapShot.val() ? querySnapShot.val() : {};
+          let list = {...data};
+          let obj = Object.values(list);
+
+          const firebaseData = obj;
+
+          console.log(',', firebaseData);
+
+          const {id, name, eventImages} = firebaseData;
+          myList.push({
+            id: id,
+            name: name,
+            eventImages: eventImages,
+          });
+
+          that.setState({
+            cards: myList,
+          });
+
+          console.log(',,', that.state.cards);
+        });
+
+      database()
         .ref('/Users/' + id)
         .on('value', querySnapShot => {
           let data = querySnapShot.val() ? querySnapShot.val() : {};
           let list = {...data};
-          console.log(list);
+          //console.log('User Data', list);
 
           this.setState({
             payment: list.payment,
@@ -306,16 +302,17 @@ class SwipeCards extends React.Component {
     }
   }
 
-  renderCard = cards => {
+  renderCard = (card, index) => {
     return (
       <View style={styles.card}>
-        <View style={{flex: 4}}>
+        <Text>{index}</Text>
+        {/* <View style={{flex: 4}}>
           <Image style={{width: '100%', height: 530}} source={cards.image} />
-        </View>
+        </View> */}
 
-        <View style={{marginTop: 0, flex: 1}}>
-          <Text style={styles.text}>{cards.text}</Text>
-        </View>
+        {/* <View style={{marginTop: 0, flex: 1}}>
+          <Text style={styles.text}>{cards.name}</Text>
+        </View> */}
       </View>
     );
   };
